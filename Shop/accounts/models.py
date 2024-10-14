58c2141d253fc86f547c001fe540d41f24cf62c2
Navid_Rahmani <navidrahmani770@gmail.com>
2024-10-14 23:21:24 +0330

    custom user model

--- Shop/accounts/models.py
+++ Shop/accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
